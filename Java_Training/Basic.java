73d1f08794b10c78308b6a20a6b42aba2399a6af
Prathima Jogannagari <p.jogannagari@zensar.com>
2022-04-23 19:51:30 +0530

    first commit

---
+++ Java_Training/Basic.java
@@ -0,0 +1,12 @@
+package Java_Training;
+
+public class Basic {
+    public static void main(String[] args) {
+        String firstName = "Prathima";
+        String lastName = "Jogannagari";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}
