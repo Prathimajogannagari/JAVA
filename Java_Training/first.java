1448b34dcb50112cc7ac94c3f8c9cd5e6b983700
Prathima Jogannagari <p.jogannagari@zensar.com>
2022-04-23 20:03:31 +0530

    Add code

---
+++ Java_Training/first.java
@@ -0,0 +1,10 @@
+package Java_Training;
+
+public class first {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 2.0;
+
+        System.out.println(a + b);
+    }
+}
