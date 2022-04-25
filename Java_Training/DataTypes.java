1a7a5c085508586e2ebcbd5c94d251423084c277
Prathima Jogannagari <p.jogannagari@zensar.com>
2022-04-25 14:44:43 +0530

    add file

---
+++ Java_Training/DataTypes.java
@@ -0,0 +1,30 @@
+package Java_Training;
+
+public package Java_Training.Trainer_Aaryan.Java_22nd;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Numbers -> Integers
+        int number = 44;
+
+        // Point, Decimal Values
+        double decimal = 99.4;
+
+        // Word, Sentences
+        String string = "Paramesh is the attendee";
+
+        // Character
+        char SingleChar = 's';
+
+        // True, False
+        Boolean bool = true;
+
+        System.out.println(number);
+        System.out.println(decimal);
+        System.out.println(string);
+        System.out.println(SingleChar);
+        System.out.println(bool);
+    }
+}class DataTypes {
+    
+}
---
+++ Java_Training/operators.java
@@ -0,0 +1,17 @@
+ Java_Training.Trainer_Aaryan.Java_22nd;
+
+    public class Operators {
+        public static void main(String[] args, boolean b) {
+            System.out.println(1 < 2); // true
+            System.out.println(1 > 2); // false
+    
+            System.out.println(2 > 2); // false
+            System.out.println(2 >= 2); // true
+    
+            System.out.println(4 == 5); // false
+            // System.out.println(4 == 4; // true
+        }
+    } 
+        
+    }
+}
