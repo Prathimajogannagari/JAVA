b21875b309dc2bc51bdc7da244e4775d333c7ec1
Prathima Jogannagari <p.jogannagari@zensar.com>
2022-05-12 15:08:44 +0530

    add file

---
+++ Java_Training/Core_Java/firstfiftyprime.java
@@ -0,0 +1,21 @@
+package Core_Java;
+
+public class firstfiftyprime {
+    public static boolean isPrime(int num) {
+        // 2 - (num-1)
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
